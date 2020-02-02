eb69a8a69e19f7d4f22850d5347cfee7bb9eedb6
Kathy Murdoch <kathy@nivan.net>
2020-02-02 11:38:56 +0000

    Basic import of a function body with no arguments.

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import ast
+
+def _import(filename):
+    with open(filename) as f:
+        basenode = ast.parse(f.read(), filename)
+    
+    funcname = os.path.splitext(os.path.basename(filename))[0]
+    fnode = ast.FunctionDef(
+        name=funcname,
+        args=ast.arguments(
+            args=[],
+            kwonlyargs=[],
+            defaults=[],
+            kw_defaults=[],
+            returns=None,
+            type_comment=None),
+        body=basenode.body,
+        decorator_list=[],
+        returns=None,
+        type_comment=None)
+    basenode.body = [fnode]
+    ast.fix_missing_locations(basenode)
+
+    code = compile(basenode, filename=filename, mode='exec')
+    exec(code)
+    return locals()[funcname]
+    
+
+f = _import('testfunc.py')
+f()
